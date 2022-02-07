e2a04ace65e0f03d1d3f79737f02d9daa2a084a4
JAMGALACTIC <jessemerriman@gmail.com>
2022-02-07 00:57:56 -0500

    removed unused references

--- YT-RED/Logging/Historian.cs
+++ YT-RED/Logging/Historian.cs
@@ -1,10 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using YT_RED.Settings;
 
 namespace YT_RED.Logging
